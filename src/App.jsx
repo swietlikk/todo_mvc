e4766d66a734291c3610a61ddf7e76ae821b9f5a
swietlikk <adamos.swietlicki@gmail.com>
2023-10-24 15:08:19 +0200

    insert todo and show todos lis

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
+import {useState} from "react";
+
 function App() {
+    const [value, setValue] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    function handleValue(event){
+        setValue(event.target.value);
+    }
+
+    function handleAddTodo(event) {
+        if (event.key === 'Enter') {
+            setTodos([...todos, value]);
+            setValue('');
+            console.log('hello sunshine!');
+        }
+    }
+
     return (
-        <h1>todos</h1>
+        <div>
+            <h1>todos</h1>
+            <input
+                type="text"
+                value={value}
+                onChange={handleValue}
+                onKeyUp={handleAddTodo}
+            />
+            <ul>
+                {todos.map((todo, id) => (
+                    <li key={id}>{todo}</li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
